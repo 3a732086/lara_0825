77798a0e4d35d5c0a09c8ba03029c545aa875a67
hu dong lin <benabcd5779@gmail.com>
2020-10-31 16:05:25 +0800

    練習4：設定Route 對應 Controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('index',[\App\Http\Controllers\PostsController::class,'index'])->name('posts.index');
+
+Route::get('post',[\App\Http\Controllers\PostsController::class,'show'])->name('posts.show');
+
+Route::get('about',[\App\Http\Controllers\PostsController::class,'about'])->name('posts.about');
+
+Route::get('contact',[\App\Http\Controllers\PostsController::class],'contact')->name('posts.contact');
