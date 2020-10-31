3f73a0697b02a653f9d21cf895ae13ab06407c45
hu dong lin <benabcd5779@gmail.com>
2020-10-31 16:40:52 +0800

    練習7：設定超連結對應 Route名稱

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,7 +33,7 @@
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
         <div class="post-preview">
-          <a href="post.blade.php">
+          <a href="{{route('posts.show')}}">
             <h2 class="post-title">
               Man must explore, and this is exploration at its greatest
             </h2>
@@ -47,7 +47,7 @@
         </div>
         <hr>
         <div class="post-preview">
-          <a href="post.blade.php">
+          <a href="{{route('posts.show')}}">
             <h2 class="post-title">
               I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
             </h2>
@@ -58,7 +58,7 @@
         </div>
         <hr>
         <div class="post-preview">
-          <a href="post.blade.php">
+          <a href="{{route('posts.show')}}">
             <h2 class="post-title">
               Science has not yet mastered prophecy
             </h2>
@@ -72,7 +72,7 @@
         </div>
         <hr>
         <div class="post-preview">
-          <a href="post.blade.php">
+          <a href="{{route('posts.show')}}">
             <h2 class="post-title">
               Failure is not an option
             </h2>
