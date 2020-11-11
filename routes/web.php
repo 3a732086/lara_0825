a91340acb117ea2fd8abb0de1dfa5cb64a392358
hu dong lin <benabcd5779@gmail.com>
2020-11-11 10:58:32 +0800

    0825 練習6：取得 Collection

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,13 @@ Route::get('/', function () {
 
     //Post::destroy(2);
 
-    Post::destroy(3,5,7);
+    //Post::destroy(3,5,7);
+
+    $allPosts=Post::all();
+    dd($allPosts);
+
+    $featuredPosts=Post::where('is_feature',1)->get();
+    dd($featuredPosts);
 
 
 });
