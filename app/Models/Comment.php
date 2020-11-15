4b04fea99a29f259945f4dd5dd24a904801c440b
hu dong lin <benabcd5779@gmail.com>
2020-11-15 19:14:27 +0800

    0825 練習7-3：在Comment.php增加屬於Post的關聯

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
 
 class Comment extends Model
 {
@@ -13,5 +14,10 @@ class Comment extends Model
     protected $fillable = [
         'content'
         ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 
 }
