93bc7f14be26f7f83b450f211bfbf1f01243ed65
hu dong lin <benabcd5779@gmail.com>
2020-11-15 19:12:07 +0800

    0825 練習7-2：在Post.php 增加 Comment 關聯

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Comment;
 
 class Post extends Model
 {
@@ -15,5 +16,10 @@ class Post extends Model
         'content',
         'is_feature'
     ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 
 }
